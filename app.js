2266ad5589ee5cf0cc87927221c2acab7fbcc678
aldi kurniawan <aldikurniawan1606@gmail.com>
2021-09-24 14:44:39 +0700

    store form on file

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const fs = require("fs")
+const cors = require('cors')
 
 const port = 5000
 
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        message:"ok"
+    })
+})
+
+app.post("/form",async(req,res)=>{
+    console.log(req.body)
+    await fs.writeFile("temp/test.txt",JSON.stringify(req.body),(err)=>{
+        if(err){
+            return res.status(500).send({
+                message:err
+            })
+        }
+    })
+    await res.status(200).send({
+        message:"ok",
+        // data:req
+    })
+})
 
 app.listen(port, () => console.log(`server is running on port: ${port}`))
